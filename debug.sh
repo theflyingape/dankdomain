93913f5283a921f522f3745c8fb34491f6885ada
Robert Hurst <theflyingape@gmail.com>
2017-10-15 17:26:55 -0400

    #Action message resizing

--- debug.sh
+++ debug.sh
@@ -3,5 +3,5 @@
 cd ./build
 killall tty.js
 node ./node_modules/tty.js/bin/tty.js --config ./tty/files/debug.json &
-firefox --new-instance -safe-mode --jsconsole --jsdebugger https://0.0.0.0 &
+firefox --new-instance -safe-mode --jsconsole --jsdebugger http://0.0.0.0:2017 &
 cd -
