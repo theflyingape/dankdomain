3c495717c801c29b30d9801dd7031054b2786746
Robert Hurst <theflyingape@gmail.com>
2017-10-31 10:41:16 -0400

    undo

--- src/tty/logon.ts
+++ src/tty/logon.ts
@@ -80,7 +80,6 @@ function who() {
         $.reroll($.player)
         $.player.keyhints = [ '','','',  '','','',  '','','',  '','','' ]
         $.newkeys($.player)
-        $.access.roleplay = false
         xvt.emulation = 'dumb'
         $.emulator(() => {
             $.player.emulation = xvt.emulation
