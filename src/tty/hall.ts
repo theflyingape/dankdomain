13a5136b1beb30fd3ec945af42b4bf14b4411f02
Robert Hurst <theflyingape@gmail.com>
2017-12-14 17:52:48 -0500

    Most memorable hits

--- src/tty/hall.ts
+++ src/tty/hall.ts
@@ -3,14 +3,18 @@
  *  HALL authored by: Robert Hurst <theflyingape@gmail.com>                  *
 \*****************************************************************************/
 
+import {sprintf} from 'sprintf-js'
+
 import $ = require('../common')
 import xvt = require('xvt')
+
 
 module Hall
 {
 	let hall: choices = {
         'F': { description:'Hall of Fame' },
         'L': { description:'Hall of Lame' },
+        'M': { description:'Most Memorable Hits' },
         'W': { description:'Past Winners List' }
 	}
 
@@ -33,6 +37,35 @@ function choice() {
     xvt.out('\n')
 
     switch (choice) {
+        case 'M':
+            xvt.out('\n')
+            xvt.out(xvt.Blue, xvt.white, '  Class      Hero                      Date      Deed       '
+                , xvt.reset, '\n')
+            xvt.out(xvt.Blue, xvt.white, '------------------------------------------------------------'
+                , xvt.reset, '\n')
+            for (let type in $.PC.name)
+                for (let pc in $.PC.name[type]) {
+                    let deeds = $.loadDeed(pc)
+                    let melee = deeds.find((x) => { return x.deed === 'melee' })
+                    if (melee)
+                        xvt.out(sprintf('%-9s  %-22s  %-11s %6d '
+                            , melee.pc, melee.hero, $.date2full(melee.date).slice(4), melee.value)
+                            , $.Deed.name[melee.deed].description, '\n')
+                    let blast = deeds.find((x) => { return x.deed === 'blast' })
+                    if (blast)
+                        xvt.out(sprintf('%-9s  %-22s  %-11s %6d '
+                            , '', blast.hero, $.date2full(blast.date).slice(4), blast.value)
+                            , $.Deed.name[blast.deed].description, '\n')
+                    let bblast = deeds.find((x) => { return x.deed === 'big blast' })
+                    if (bblast)
+                        xvt.out(sprintf('%-9s  %-22s  %-11s %6d '
+                            , '', bblast.hero, $.date2full(bblast.date).slice(4), bblast.value)
+                            , $.Deed.name[bblast.deed].description, '\n')
+                    if (deeds.length) xvt.out('\n')
+                }
+            suppress = true
+            break
+
         case 'Q':
 			require('./main').menu($.player.expert)
 			return
@@ -40,7 +73,7 @@ function choice() {
         case 'W':
             xvt.out(xvt.magenta, '\n        --=:)) Past Winners List ((:=--', xvt.reset, '\n\n')
             $.cat('winners')
-            suppress = false
+            suppress = true
             break
     }
 	menu(suppress)
