c459ed3b5c41936814589154d772c8b6bb04881f
Robert Hurst <theflyingape@gmail.com>
2017-12-22 16:04:13 -0500

    no mana potions for no-mana players

--- src/tty/dungeon.ts
+++ src/tty/dungeon.ts
@@ -1700,8 +1700,8 @@ function generateLevel() {
 			n = $.dice(130 - deep)
 			for (let i = 0; i < 16 && n > 0; i++) {
 				DL.rooms[y][x].giftValue = 15 - i
-				if ($.player.magic < 2 && DL.rooms[y][x].giftValue > 2 && DL.rooms[y][x].giftValue < 5)
-					DL.rooms[y][x].giftValue >>= 1
+				if ($.player.magic < 2 && DL.rooms[y][x].giftValue > 1 && DL.rooms[y][x].giftValue < 4)
+					DL.rooms[y][x].giftValue -= 2
 				n -= i + 1
 			}
 			continue
