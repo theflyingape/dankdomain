897713a32b289fe4c64ad85493433a47288e6e22
Robert Hurst <theflyingape@gmail.com>
2018-09-06 08:17:59 -0400

    maintenance

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #
-# transpile into the server-side middleware app and bundle for the client-side browser
+# transpile into the server-side middleware app
+# and bundle for the client-side browser app
 #
 tsc -p ./src --outDir ./build
 browserify ./build/door/client.js -o ./build/door/static/bundle.js
