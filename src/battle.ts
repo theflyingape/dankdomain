4e4078be0d0176ddfb5e791fa7b165ba116a3f05
Robert Hurst <theflyingape@gmail.com>
2019-04-27 16:18:19 -0400

    bump

--- src/battle.ts
+++ src/battle.ts
@@ -274,10 +274,10 @@ export function attack(retry = false) {
             && $.dice(enemy.user.level / 9 + 15) > $.int(rpc.user.level / 9 + 3)
             //  d[22-32] > 11-21, coin-flip typical, true: 5% min - 66% max
             && $.dice((enemy.dex > 90 ? enemy.dex - 89 : 1) + 21) > ((rpc.dex > 90 ? rpc.dex - 89 : 1) + 10)
-            //  d[4-12(+rings)] > 3d[6], 1% always false, mean chance to be true for a master: 14%
-            //  max chance to be true for lawful: 1%, desperate: 11%, trickster: 50%, adept: 67%, master: 92%
-            && $.dice(2 * (enemy.user.steal + $.Ring.power(rpc.user.rings, enemy.user.rings, 'steal').power) + 4)
-             > ($.dice(6) + $.dice(6) + $.dice(6)))
+            //  d[0-8(+rings)] + 2 > 2d[6], true min 3%,
+            //  max chance to be true for lawful: 3%, desperate: 6%, trickster: 17%, adept: 67%, master: 92%
+            && $.dice(2 * (enemy.user.steal + $.Ring.power(rpc.user.rings, enemy.user.rings, 'steal').power))
+             > ($.dice(6) + $.dice(6) - 2))
             skip.power = 1
         if (skip.power) {
             let how = enemy.pc.skip || 'kiss', color = enemy.pc.color || xvt.white
