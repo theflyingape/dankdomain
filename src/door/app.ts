b210b1879f7690c00f52f90de68cf43864ec8264
Robert Hurst <theflyingape@gmail.com>
2019-07-03 09:51:53 -0400

    bump

--- src/door/app.ts
+++ src/door/app.ts
@@ -66,7 +66,7 @@ dns.lookup('localhost', (err, addr, family) => {
     let list = []
     list.push(
       { handle:`The <span style="color:brown !important;">Player manual</span>: <a href="https://ddgame.us" target="_blank">DDgame.us</a>`, png:'connect/arexielite_by_peachyco', effect:'fadeInRight' },
-      { handle:'100 dungeon levels', png:'connect/dragonborn_dagger_sorcerer_by_peachyco', effect:'fadeInRight' },
+      { handle:'100 dungeon levels, 10 dank domains', png:'connect/dragonborn_dagger_sorcerer_by_peachyco', effect:'fadeInRight' },
       { handle:'Take on the Monster Mash', png:'connect/dragonborn_hexblade_by_peachyco', effect:'fadeInRight' },
       { handle:'Come get some', png:'connect/elf_cleric_by_peachyco', effect:'fadeInRight' },
       { handle:'Crying will not help you', png:'connect/elf_fighter_by_peachyco', effect:'fadeInRight' },
@@ -89,7 +89,7 @@ dns.lookup('localhost', (err, addr, family) => {
     const armor = require('../items/armor.json')
     for (let i in armor) {
       if (!armor[i].armoury) {
-        let profile = { handle:`<span style="color:brown !important;">${i}</span>`, level:armor[i].ac, pc:'armor', effect:'fadeInUpBig' }
+        let profile = { handle:`<span style="color:${armor[i].dwarf ? 'black' : 'brown'} !important;">${i}</span>`, level:armor[i].ac, pc:(armor[i].dwarf ? 'dwarven' : 'uncommon') + ' armor', effect:'fadeInUpBig' }
         profile['jpg'] = `specials/${i}`
         list.push(profile)
       }
@@ -98,7 +98,7 @@ dns.lookup('localhost', (err, addr, family) => {
     const weapon = require('../items/weapon.json')
     for (let i in weapon) {
       if (!weapon[i].shoppe) {
-        let profile = { handle:`<span style="color:brown !important;">${i}</span>`, level:weapon[i].wc, pc:'weapon', effect:'fadeInUpBig' }
+        let profile = { handle:`<span style="color:${weapon[i].dwarf ? 'black' : 'brown'} !important;">${i}</span>`, level:weapon[i].wc, pc:(weapon[i].dwarf ? 'dwarven' : 'uncommon') + ' weapon', effect:'fadeInUpBig' }
         profile['jpg'] = `specials/${i}`
         list.push(profile)
       }
@@ -109,12 +109,8 @@ dns.lookup('localhost', (err, addr, family) => {
     for (let n in monsters) {
       let pc = monsters[n].pc
       let profile = { handle:`The <span style="color:brown !important;">${n}</span>:`, level:++level, pc:pc, effect:monsters[n].effect || 'fadeIn' }
-      if (pc)
-        profile['jpg'] = `dungeon/${n}`
-      else {
-        profile['png'] = `monster/monster`
-        profile['pc'] = `same class`
-      }
+      profile['jpg'] = `dungeon/${n}`
+      if (!pc) profile['pc'] = `(same class)`
       list.push(profile)
     }
 
@@ -140,7 +136,7 @@ dns.lookup('localhost', (err, addr, family) => {
 
     const ring = require('../items/ring.json')
     for (let i in ring) {
-      let profile = { handle:`The <span style="color:brown !important;">${i}</span> ${ring[i].emoji} ring:`, pc:ring[i].description, effect:'fadeInUpBig' }
+      let profile = { handle:`<h2>${ring[i].unique ? 'The <span style="color:black' : 'Special <span style="color:brown'} !important">${i}</span> ${ring[i].emoji} ring</h2>`, pc:ring[i].description, effect:'fadeInUpBig' }
       profile['jpg'] = `ring/${i}`
       list.push(profile)
     }
