7f72c899ec4c40b2fb8ae651140f36cbe8e75656
Robert Hurst <rhurst@rampage.home>
2020-01-31 09:19:09 -0500

    bump

--- src/common.ts
+++ src/common.ts
@@ -1983,7 +1983,7 @@ module Common {
 
     export function death(by: string) {
         reason = by
-        profile({ png: `death${player.today}`, effect: 'fadeInDownBig' })
+        profile({ handle: reason, png: `death${player.today}`, effect: 'fadeInDownBig' })
     }
 
     //  render a menu of options and return the prompt
