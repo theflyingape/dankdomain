e1dddadaee693d6d4f83e836680641dd7f38dde5
Robert Hurst <theflyingape@gmail.com>
2020-08-12 08:05:55 -0400

    chromeos fixes

--- build/tty.sh
+++ build/tty.sh
@@ -3,6 +3,7 @@
 path=`dirname $0`; cd $path || exit 1
 export HOME=$PWD/door
 umask 0002
-eval `resize | grep LINES`
+eval `resize 2> /dev/null | grep LINES`
+[ -n "$LINES" ] || export LINES=25
 
 exec node telnet localhost 1939 $LINES
