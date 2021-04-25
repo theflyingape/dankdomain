1c10880ea3ef4ab9f4a0df515c18cc58768d21f2
Robert Hurst <theflyingape@gmail.com>
2021-04-25 09:42:48 -0400

    bump

--- upgrade.sh
+++ upgrade.sh
@@ -3,16 +3,22 @@
 [ -n "$1" ] && TARGET="$1" || TARGET=/usr/local/games
 TARGET="${TARGET}/`basename ${PWD}`"
 
-# let's prompt for admin credentials now, if necessary
-git pull || exit
-
 sudo systemctl stop dankdomain-door
+npm install
+npm run build
+
+sudo rsync -av --chown=root:games ./build/ ${TARGET}
+sudo rsync -av --chown=root:games package.json ${TARGET}
 sudo chown -R root.games ${TARGET}
 sudo chmod -R u+rw,g+rw,o-rwx ${TARGET}
 sudo find ${TARGET} -type d -exec chmod u+x,g+xs {} \;
 
+#echo ''
+#echo 'server files that exist that are not part of this build'
+#echo '                  ~~~~~          ~~~'
+#sudo rsync -anv --delete --exclude node_modules --exclude files ./build/ ${TARGET}
+
 cd ${TARGET}
-rm package-lock.json
 npm install
 
 # xterm door service
